24478d4d45db73b5014a466eb127357d7469314e
Alexandru Maries <alexandru.maries@icloud.com>
2022-11-25 09:16:30 +0200

    Adds meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
-import AboutMe from '../components/AboutMe/AboutMe';
-import style from '../styles/index.module.css';
-import Wallpaper from '../components/Wallpaper/Wallpaper';
+import AboutMe from "../components/AboutMe/AboutMe";
+import style from "../styles/index.module.css";
+import Wallpaper from "../components/Wallpaper/Wallpaper";
+import Head from "next/head";
 
-export default function Home(props)
-{
-    const basicInfo = [
-        {Type: "date of birth", Info: "August 2"},
-        {Type: "email", Info: "alexandru.maries@icloud.com"},
-        {Type: "address", Info: "Cluj-Napoca, Romania"},
-        {Type: "languages", Info: "Romanian, English"},
-    ];
+export default function Home(props) {
+  const basicInfo = [
+    { Type: "date of birth", Info: "August 2" },
+    { Type: "email", Info: "alexandru.maries@icloud.com" },
+    { Type: "address", Info: "Cluj-Napoca, Romania" },
+    { Type: "languages", Info: "Romanian, English" },
+  ];
 
-    return (
-        <div className={style.container}>
-            <Wallpaper></Wallpaper>
-            <AboutMe Infos={basicInfo}></AboutMe>
-        </div>
-    );
+  return (
+    <>
+      <Head>
+        <title>Alexandru Maries</title>
+        <meta name="title" content="Alexandru Maries"/>
+        <meta name="description" content="Alexandru Maries personal portofolio"/>
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className={style.container}>
+        <Wallpaper></Wallpaper>
+        <AboutMe Infos={basicInfo}></AboutMe>
+      </div>
+    </>
+  );
 }
